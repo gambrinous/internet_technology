dbb893cff8e2004674701110ba0d8fc41092d9c6
halfgoku <m-alaks@hotmail.com>
2014-03-01 18:49:31 +0000

    urls

--- rate/views.py
+++ rate/views.py
@@ -8,15 +8,18 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from datetime import datetime
 
+
 def index(request):
     context = RequestContext(request)
     context_dict = {'boldmessage': "hello world!"}
     return render_to_response('rate/index.html', context_dict, context)
 
+
 def about(request):
     context = RequestContext(request)
     context_dict = {'boldmessage': "Here is the about page."}
     return render_to_response('rate/about.html', context_dict, context)
+
 
 def contact(request):
     context = RequestContext(request)
