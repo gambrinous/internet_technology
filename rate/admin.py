52ae08b718126f2c43120602e5c31ee10f720fb9
hectorparad <hectorparad@gmail.com>
2014-03-01 21:12:57 +0000

    populate scr

--- rate/admin.py
+++ rate/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from rate.models import University, Student, Course, Rate, UniCourse
+from rate.models import University, Student, Course, Rating, UniCourse
 
 admin.site.register(University)
 admin.site.register(Student)
 admin.site.register(Course)
-admin.site.register(Rate)
+admin.site.register(Rating)
 admin.site.register(UniCourse)
