a089a3b3b502e03657b7f11ce645cd595c9a6834
hectorparad <hectorparad@gmail.com>
2014-03-05 16:16:03 +0000

    Merge branch 'master' of H:\workspace\ITECH\RateMyCourse with conflicts.

--- rate/urls.py
+++ rate/urls.py
@@ -6,4 +6,7 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
     url(r'^contact/$', views.contact, name='contact'),
+    url(r'^restricted/$', views.restricted, name='restricted'),
+    url(r'^underConstruction/$', views.underConstruction, name='underConstruction'),
+    url(r'^register/$', views.register, name='register'),
 )
