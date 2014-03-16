41d7fc4997a2164e229633e4cf6c628fcbe02213
Marios <2097434f@student.gla.ac.uk>
2014-03-16 17:56:43 +0000

    about

--- rate/views.py
+++ rate/views.py
@@ -16,13 +16,15 @@ def index(request):
         level_option = request.POST['level_options']
         year_option = request.POST['year_options']
 
-        the_course = Course.objects.filter(university=university_option).filter(title=course_option).filter(level=level_option).filter(year=year_option)
+        the_course = Course.objects.filter(university=university_option).filter(title=course_option).filter(
+            level=level_option).filter(year=year_option)
         if the_course:
             for j in the_course:
-                j.url = j.title.replace(' ','_')
-            return HttpResponseRedirect('course/'+j.url)
+                j.url = j.title.replace(' ', '_')
+            return HttpResponseRedirect('course/' + j.url)
         else:
-            return HttpResponse('There is no such course. Please <a href="/rate/contact/">contact</a> the administrator to request it, or go back to <a href="/rate/">main page</a>.')
+            return HttpResponse(
+                'There is no such course. Please <a href="/rate/contact/">contact</a> the administrator to request it, or go back to <a href="/rate/">main page</a>.')
     else:
         top_five_list = Course.objects.order_by('-stored_average_rating')[:5]
         worst_five_list = Course.objects.order_by('stored_average_rating')[:5]
@@ -65,7 +67,8 @@ def user_login(request):
                     login(request, user)
                     return HttpResponseRedirect('/rate/')
                 else:
-                    return HttpResponse("Sorry your account is not enabled. Your University's domain is not confirmed yet.")
+                    return HttpResponse(
+                        "Sorry your account is not enabled. Your University's domain is not confirmed yet.")
             else:
                 print "Invalid login details: {0}, {1}".format(username, password)
                 return HttpResponse("Invalid login details supplied.")
@@ -73,6 +76,7 @@ def user_login(request):
             return render_to_response('rate/login.html', {}, context)
     else:
         return HttpResponse('You are already signed in. Go back to <a href="/rate/">main page</a>.')
+
 
 @login_required
 def user_logout(request):
@@ -83,25 +87,27 @@ def user_logout(request):
 def register(request):
     if not request.user.is_authenticated():
         context = RequestContext(request)
-        return render_to_response('rate/register.html', context)
+        registered = False
         if request.method == 'POST':
             temp = request.POST['email']
-            return HttpResponse(temp)
-        # registered = False
-        # if request.method == 'POST':
-        #     user_form = UserForm(data=request.POST)
-        #     if user_form.is_valid():
-        #         user = user_form.save()
-        #         user.set_password(user.password)
-        #         user.username = user.email
-        #         user.save()
-        #         registered = True
-        #         return HttpResponseRedirect('/rate/login')
-        #     else:
-        #         print user_form.errors,
-        # else:
-        #     user_form = UserForm()
-        # return render_to_response('rate/register.html', {'user_form': user_form, 'registered': registered}, context)
+            domain_list = University.objects.order_by('domain')
+            for i in domain_list:
+                if i.domain in temp:
+                    user_form = UserForm(data=request.POST)
+                    if user_form.is_valid():
+                        user = user_form.save()
+                        user.set_password(user.password)
+                        user.username = user.email
+                        user.save()
+                        registered = True
+                        return HttpResponseRedirect('/rate/login')
+                    else:
+                        print user_form.errors,
+                else:
+                    return HttpResponse('This university is not in our database yet.')
+        else:
+            user_form = UserForm()
+        return render_to_response('rate/register.html', {'user_form': user_form, 'registered': registered}, context)
     else:
         return HttpResponse('You are already registered and signed in. Go back to <a href="/rate/">main page</a>.')
 
@@ -116,7 +122,7 @@ def restricted(request):
 def sitemap(request):
     context = RequestContext(request)
     list = Course.objects.order_by('title')
-    context_dict = {'list': list,}
+    context_dict = {'list': list, }
     for course in list:
         course.url = course.title.replace(' ', '_')
     return render_to_response('rate/sitemap.html', context_dict, context)
@@ -138,7 +144,7 @@ def course(request, course_title_url):
                 if r.student.email == request.user.email:
                     flag = "no"
 
-            domain = request.user.email.split("@",1)[1]
+            domain = request.user.email.split("@", 1)[1]
             if course.university.domain != domain:
                 flag = "no"
         context_dict['rateIt'] = flag
@@ -155,15 +161,18 @@ def rated_courses(request, type):
         level_option = request.POST['level']
         year_option = request.POST['year']
         if type == "top":
-            list = Course.objects.filter(university=university_option, level=level_option, year=year_option).order_by('-stored_average_rating')[:20]
+            list = Course.objects.filter(university=university_option, level=level_option, year=year_option).order_by(
+                '-stored_average_rating')[:20]
             title = "Top Rated Courses"
             choose_tit = 1
         if type == "worst":
-            list = Course.objects.filter(university=university_option, level=level_option, year=year_option).order_by('stored_average_rating')[:20]
+            list = Course.objects.filter(university=university_option, level=level_option, year=year_option).order_by(
+                'stored_average_rating')[:20]
             title = "Worst Rated Courses"
             choose_tit = 2
         if type == "latest":
-            list = Course.objects.filter(university=university_option, level=level_option, year=year_option).order_by('-date')[:20]
+            list = Course.objects.filter(university=university_option, level=level_option, year=year_option).order_by(
+                '-date')[:20]
             title = "Most Recent Rated Courses"
             choose_tit = 3
     else:
@@ -186,7 +195,7 @@ def rated_courses(request, type):
     rates_list = Rate.objects.values('course', 'date')[:1]
     context_dict = {'list': list, 'title': title,
                     'universitylist': university_list, 'courselist': course_list, 'yearlist': year_list,
-                    'rateslist': rates_list, 'ch_title':choose_tit}
+                    'rateslist': rates_list, 'ch_title': choose_tit}
     for rate in list:
         rate.url = rate.title.replace(' ', '_')
 
@@ -199,7 +208,7 @@ def rateIt(request, course_title_url):
 
         # add to db
 
-        return HttpResponseRedirect('/rate/course/'+course_title_url)
+        return HttpResponseRedirect('/rate/course/' + course_title_url)
     else:
         if request.user.is_authenticated():
             course_title = course_title_url.replace('_', ' ')
