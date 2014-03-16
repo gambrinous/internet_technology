b1b85dace543e3fd80aa8358a200084289ffcc71
hectorparad <hectorparad@gmail.com>
2014-03-16 12:09:19 +0000

    ico

--- RateMyCourse/urls.py
+++ RateMyCourse/urls.py
@@ -8,4 +8,5 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rate/', include('rate.urls')),
+    url(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/images/ico/favicon.ico'}),
 )
